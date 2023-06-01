afed2eba4e7b2987f4a48086b782d4182f952e8f
Adam Derylo <a.m.derylo@gmail.com>
2023-06-01 22:59:01 +0200

    Find paths

--- wyprawy.pl
+++ wyprawy.pl
@@ -1,4 +1,17 @@
-% Przykładowy program główny w Prologu
+% Przykładowy program głowny w Prologu
+
+trasa(r1, zakopane, brzeziny, rower, oba, 25).
+trasa(r2, brzeziny, gasienicowa, rower, oba, 15).
+trasa(r3, brzeziny, poroniec, rower, oba, 10).
+trasa(r4, poroniec, rusinowa, rower, oba, 6).
+trasa(g1, zakopane, kuznice, gorska, oba, 7).
+trasa(g2, zakopane, kalatowki, gorska, oba, 5).
+trasa(g3, kuznice, gasienicowa, gorska, oba, 7).
+trasa(g4, gasienicowa, zawrat, gorska, oba, 6).
+trasa(g5, gasienicowa, czarnystaw, gorska, oba, 3).
+trasa(g6, zawrat, kozia, gorska, jeden, 5).
+trasa(g7, kozia, gasienicowa, gorska, jeden, 7).
+trasa(p1, zakopane, gubalowka, piesza, oba, 5).
 
 user:runtime_entry(start):-
     (current_prolog_flag(argv, [File]) ->
@@ -27,3 +40,12 @@ przetwarzaj :-
     ;
       format('Brak trasy z ~p do ~p.~n', [Start, Meta])
     ).
+
+findPaths(From, To, Path):-
+  findPaths(From, To, [], Path).
+
+findPaths(X, X, T, T).
+findPaths(X, Y, T, NT) :-
+    trasa(_Id, X, Z, _Type, _Dir, _Len), 
+    \+ member(Z,T),
+    findPaths(Z, Y, [Z|T], NT).   
