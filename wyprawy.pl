c34b607c4f236caba75d667ebdf74e3253f30d95
ad432952 <a.m.derylo@gmail.com>
2023-06-02 15:29:45 +0200

    input conds fix

--- wyprawy.pl
+++ wyprawy.pl
@@ -1,21 +1,21 @@
 % Przykładowy program głowny w Prologu
 
-trasa(r1, zakopane, brzeziny, rower, oba, 25).
-trasa(r2, brzeziny, gasienicowa, rower, oba, 15).
-trasa(r3, brzeziny, poroniec, rower, oba, 10).
-trasa(r4, poroniec, rusinowa, rower, oba, 6).
-trasa(g1, zakopane, kuznice, gorska, oba, 7).
-trasa(g2, zakopane, kalatowki, gorska, oba, 5).
-trasa(g3, kuznice, gasienicowa, gorska, oba, 7).
-trasa(g4, gasienicowa, zawrat, gorska, oba, 6).
-trasa(g5, gasienicowa, czarnystaw, gorska, oba, 3).
-trasa(g6, zawrat, kozia, gorska, jeden, 5).
-trasa(g7, kozia, gasienicowa, gorska, jeden, 7).
-trasa(p1, zakopane, gubalowka, piesza, oba, 5).
-trasa(s1, warszawa, krakow, rower, oba, 50).
-trasa(s2, warszawa, czestochowa, rower, oba, 100).
-trasa(s3, krakow, jura, rower, oba, 30). 
-trasa(s4, jura, czestochowa, piesza, oba, 40). 
+% trasa(r1, zakopane, brzeziny, rower, oba, 25).
+% trasa(r2, brzeziny, gasienicowa, rower, oba, 15).
+% trasa(r3, brzeziny, poroniec, rower, oba, 10).
+% trasa(r4, poroniec, rusinowa, rower, oba, 6).
+% trasa(g1, zakopane, kuznice, gorska, oba, 7).
+% trasa(g2, zakopane, kalatowki, gorska, oba, 5).
+% trasa(g3, kuznice, gasienicowa, gorska, oba, 7).
+% trasa(g4, gasienicowa, zawrat, gorska, oba, 6).
+% trasa(g5, gasienicowa, czarnystaw, gorska, oba, 3).
+% trasa(g6, zawrat, kozia, gorska, jeden, 5).
+% trasa(g7, kozia, gasienicowa, gorska, jeden, 7).
+% trasa(p1, zakopane, gubalowka, piesza, oba, 5).
+% trasa(s1, warszawa, krakow, rower, oba, 50).
+% trasa(s2, warszawa, czestochowa, rower, oba, 100).
+% trasa(s3, krakow, jura, rower, oba, 30). 
+% trasa(s4, jura, czestochowa, piesza, oba, 40). 
 ensure_loaded(library(lists)).
 
 
@@ -39,17 +39,20 @@ przetwarzaj :-
   write('Podaj koniec: '),
   read(End),
   write('Podaj warunki: '),
+  read(Conditions), 
   evalConditions(Conditions, Types, LenCondtion),
+  write('conds all right'), 
   (
     findPaths(Start, End, Types, LenCondtion, Path) ->
     (
-      format('Istnieje trasa dlugosci ~d.~n', [Km]),
+      % format('Istnieje trasa dlugosci ~d.~n', [Km]),
       write('Trasa: '), write(Path), nl 
     );
     format('Brak trasy z ~p do ~p.~n', [Start, End])
   ).
 
 % === input parsing =================================================================
+evalConditions(nil, nil, (ge, 0)).
 
 evalConditions(Conditions, Types, LenCondtions) :-
   tupleToList(Conditions, ConditionsList),
